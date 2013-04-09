51176dbadf239af0338a78ce4218aa3670bb0fa5
luisansal <luis.sanchez.saldana@gmail.com>
2013-04-08 23:02:46 -0500

    adding files

---
+++ EsolvingEschoolThirdSonataBlockBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Esolving\Eschool\Third\SonataBlockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EsolvingEschoolThirdSonataBlockBundle extends Bundle {
+
+    public function getParent() {
+        return 'SonataBlockBundle';
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 EsolvingEschoolThirdSonataBlockBundle
 =====================================
 
-readme
+Is a bundle to override some templates and behaviors inside our SonataBlockBundle in School Project.
